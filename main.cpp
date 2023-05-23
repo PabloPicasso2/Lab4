d7a1151d53d13ae3f549408ab670ecc9f2ce7c5f
PabloPicasso2 <130559374+PabloPicasso2@users.noreply.github.com>
2023-05-23 11:36:57 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <cmath>
 
 struct Signal {
-    virtual int generate() { return 0; }
+    virtual double generate() { return 1.0; }
 };
 
 struct SquareDecorator : Signal {
    private:
     Signal* m_sig;
 
-    int generate_square() { return 1; }
+    // return +amplitude or -amplitude
+    double generate_square() { return 2.0; }
+    // double generate_square() { return -2.0; }
 
    public:
     SquareDecorator(Signal* const sig) : m_sig(sig) {}
-    int generate() override {
+    double generate() override {
         std::cout << "Decorating with square\n";
         return m_sig->generate() + generate_square();
     }
@@ -23,13 +26,14 @@ struct SquareDecorator : Signal {
 struct SineDecorator : Signal {
    private:
     Signal* m_sig;
-    
-    // output = m_amplitude * sin(2 * 3.14159 *m_time / m_period);
-    int generate_sine() { return 2; }
+
+    // return 0.0 if you don't want to decorate by sinus
+    double generate_sine() { return 0.0; }
+    //double generate_sine() { return sin(2.0 * 3.14159 /12.0); }
 
    public:
     SineDecorator(Signal* const sig) : m_sig(sig) {}
-    int generate() override {
+    double generate() override {
         std::cout << "Decorating with sine\n";
         return m_sig->generate() + generate_sine();
     }
@@ -39,12 +43,12 @@ struct TriangleDecorator : Signal {
    private:
     Signal* m_sig;
 
-    // output = (2 * m_amplitude / 3.14) * asin(sin(2 * 3.14 * m_time / m_period));
-    int generate_triangle() { return 3; }
+    //double generate_triangle() { return 0.0; }
+    double generate_triangle() { return (2 * 1 / 3.14) * asin(sin(2 * 3.14 * 1 / 12)); }
 
    public:
     TriangleDecorator(Signal* const sig) : m_sig(sig) {}
-    int generate() override {
+    double generate() override {
         std::cout << "Decorating with triangle\n";
         return m_sig->generate() + generate_triangle();
     }
@@ -52,6 +56,7 @@ struct TriangleDecorator : Signal {
 
 int main() {
     Signal sig;
+    // decorate by all additional signals
     SineDecorator sine_dec(&sig);
     TriangleDecorator tr_dec(&sine_dec);
     SquareDecorator sq_dec(&tr_dec);
