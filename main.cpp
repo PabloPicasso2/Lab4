36440b111b3c7584b5853ea90e5477b584e8e463
PabloPicasso2 <130559374+PabloPicasso2@users.noreply.github.com>
2023-05-23 09:22:20 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main()
 {
     Comp_Gen *generator = new Comp_Concrete(0.0);
     double period1 = 12.0, amplitude1 = 1.0;
-    // period = 10.0, amplitude1 = 1.0
+    // Dec_square: period = 10.0, amplitude1 = 1.0
     //generator = new Dec_square(generator, amplitude1, period1);
-    // period = 12.0, amplitude1 = 1.0
+    // Dec_sinus: period = 12.0, amplitude1 = 1.0
     //generator = new Dec_sinus(generator, amplitude1, period1);
-    // period = 12.0, amplitude1 = 1.0
+    // Dec_triangle: period = 12.0, amplitude1 = 1.0
     generator = new Dec_triangle(generator, amplitude1, period1);
 
     for (int i = 0; i < 2*period1; ++i)
